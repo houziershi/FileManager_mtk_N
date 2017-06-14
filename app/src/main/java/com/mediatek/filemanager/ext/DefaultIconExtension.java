e6a66266010db15d126e29d0a7ebccfcee9d82dd
guokun.hou <hgk324@163.com>
2017-06-14 22:12:03 +0800

    code base

---
+++ app/src/main/java/com/mediatek/filemanager/ext/DefaultIconExtension.java
@@ -0,0 +1,25 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.mediatek.filemanager.ext;
+
+import android.graphics.Bitmap;
+import com.mediatek.filemanager.ext.IIconExtension;
+
+public class DefaultIconExtension implements IIconExtension {
+    public DefaultIconExtension() {
+    }
+
+    public void createSystemFolder(String defaultPath) {
+    }
+
+    public Bitmap getSystemFolderIcon(String path) {
+        return null;
+    }
+
+    public boolean isSystemFolder(String path) {
+        return false;
+    }
+}
---
+++ app/src/main/java/com/mediatek/filemanager/ext/IIconExtension.java
@@ -0,0 +1,16 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.mediatek.filemanager.ext;
+
+import android.graphics.Bitmap;
+
+public interface IIconExtension {
+    void createSystemFolder(String var1);
+
+    boolean isSystemFolder(String var1);
+
+    Bitmap getSystemFolderIcon(String var1);
+}
